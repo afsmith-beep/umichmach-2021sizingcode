0401648efe6e92a140941cf6008e71bc5cb74d7f
Tyler <tpangerl@umich.edu>
2020-10-05 15:11:32 -0400

    Minor Take Off Syntax Updates
    Minor updates to air density variable name and minor syntax corrections

--- TakeOffDis.m
+++ TakeOffDis.m
@@ -1,6 +1,6 @@
-function [takeoff_dist] = TakeOffDis (AR, mu, thrust_to_weight, MTOW, airdensity, Takeoff_velocity, wing_ref_area, Cl_takeoff, CD_0 )
+function [takeoff_dist] = TakeOffDis (AR, mu, thrust_to_weight, MTOW, air_density, Takeoff_velocity, wing_ref_area, Cl_takeoff, CD_0 )
     % Takeoff Distance: Raymer 487
-    K = 1 ./ (pi * e * AR)
+    K = 1 ./ (pi * e * AR);
     %Kt = thrust_to_weight - mu
     Kt = thrust ./ MTOW - mu;
     Ka = air_density ./ 2 ./ (MTOW ./ wing_ref_area) .* (mu .* Cl_takeoff - CD_0 - K  .* Cl_takeoff .^2);
